805d77ab0d9da6272a7751dd6b7889b2cf240689
root <redrenjer@yandex.ru>
2024-03-06 10:34:09 +0300

    Initial Project

---
+++ app/src/main/java/com/fragments/app/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.fragments.app
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.fragments.app.abcd_part.ABCDActivity
+import com.fragments.app.databinding.ActivityMainBinding
+import com.fragments.app.users_list_part.ContactsActivity
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        initView()
+    }
+
+   private fun initView() {
+
+    }
+
+    private fun <T> moveToActivity(activity: Class<T>) {
+        val intent = Intent(this, activity)
+        startActivity(intent)
+    }
+}
